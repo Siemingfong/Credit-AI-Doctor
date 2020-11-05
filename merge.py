f7bec77d531db8fb75186904e552d65925913f98
Bigmoumou <o1r2g3a4n5i6z7e8@gmail.com>
2020-11-05 18:09:37 +0800

    add full industry_all_merge.csv with full en_name & merged ranking

--- merge.py
+++ merge.py
@@ -4,54 +4,50 @@ import pandas as pd
 import requests
 
 
-def replace_all_df(text, lis):
-    for i in lis:
-        text = text.str.replace(i, '')
+def replace_all_df(text, lis, mode):
+    if mode == 'list':
+        for i in lis:
+            text = text.str.replace(i, '')
+    elif mode == 'dict':
+        for k, v in lis.items():
+            text = text.str.replace(k, v)
     return text
 
-def replace_all(text, lis):
-    for i in lis:
-        text = text.replace(i, '')
+def replace_all(text, lis, mode):
+    if mode == 'list':
+        for i in lis:
+            text = text.replace(i, '')
+    elif mode == 'dict':
+        for k, v in lis.items():
+            text = text.replace(k, v)
     return text
 
-def preprocess_twrating_cns(df, rep):
-    cns = df['en_name'].str.lower().str.strip()
-    df['en_name'] = replace_all_df(cns, rep)
+def preprocess_twrating_cns(df, rep, which=None, mode='list'):
+    cns = df[which].str.lower().str.strip()
+    df['merge_key'] = replace_all_df(cns, rep, mode)
     return df
-    
-# def main(params):
-#     df1 = pd.read_csv(params['mops_data'])
-#     df2 = pd.read_csv(params['twrating_data'])
-    
-#     rep = ['corporation', 'corp', 'ltd', 'co', ',', '.', ' ', '&']
-#     df2 = preprocess_twrating_cns(df2, rep)
-#     print(df2['entity'])
-#     for idx, row in df1.iterrows():
-#         cn = row['en_name'].lower().strip()
-#         cn = replace_all(cn, rep)
-#         # print(cn)
-#         if row['證券代號'] == 1303:
-#             print(cn)
-#             print(df2['entity'].loc[0])
-#         if cn in list(df2['entity']):
-#             print('Yoooooo!')
 
 def main(params):
     df1 = pd.read_csv(params['mops_data'])
     df2 = pd.read_csv(params['twrating_data'])
     
-    rep = ['corporation', 'corp', 'ltd', 'co', ',', '.', ' ', '&']
-    df1 = preprocess_twrating_cns(df1, rep)
-    print(df1)
+    rep1 = ['corporation', 'financial', 'industry', 'company', 'limited',
+           'corp', 'ltd', 'inc', 'ind', 'the', 'co.', 
+           ',', '.', ' ', '&', '(', ')']
+    rep2 = {'holdings': 'holding'}
+    # process twrating keys (df1)
+    df1 = preprocess_twrating_cns(df1, rep1, 'en_name', mode='list')
+    df1 = preprocess_twrating_cns(df1, rep2, 'merge_key', mode='dict')
+    # print(df1)
 
-    for idx, row in df2.iterrows():
-        cn = row['entity'].lower().strip()
-        cn = replace_all(cn, rep)
-
-        if cn in list(df1['en_name']):
-            print('Yoooooo!')
-        else:
-            print(cn)
+    # process mops keys (df2)
+    df2 = preprocess_twrating_cns(df2, rep1, 'entity', mode='list')
+    df2 = preprocess_twrating_cns(df2, rep2, 'merge_key', mode='dict')
+    # print(df2)
+    
+    df2 = df2.merge(df1, how='right', left_on='merge_key', right_on='merge_key')
+    df2 = df2.drop(['entity', 'merge_key'], axis=1)
+    df2.to_csv('industry_all_merge.csv', index=False, encoding='utf-8-sig')
             
 
 if __name__ == '__main__':
